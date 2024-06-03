bf4389dfc310d4dcf967413c4ceb5016f7ca964b
EmanAmr1 <emanamr543@gmail.com>
2024-06-03 13:20:03 +0300

    pages security

---
+++ stickynotes/src/main/java/com/springmvcproject/stickynotes/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.springmvcproject.stickynotes.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authRequest -> {
+            authRequest.requestMatchers("/about", "/css/**").permitAll();
+            authRequest.requestMatchers("/","/note/**","/my-notes/**").authenticated();
+
+        });
+        http.formLogin(Customizer.withDefaults());
+        http.httpBasic(Customizer.withDefaults());
+        return  http.build();
+    }
+
+}
